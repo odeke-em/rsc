bfa173b1b4408d958d186931388d6a7aeecf7096
Emmanuel Odeke <odeke@ualberta.ca>
2015-10-16 19:09:13 -0600

    qr: moved imports to new github.com/odeke-em

--- qr/web/play.go
+++ qr/web/play.go
@@ -44,11 +44,11 @@ import (
 	"strings"
 	"time"
 
-	"code.google.com/p/rsc/appfs/fs"
-	"code.google.com/p/rsc/gf256"
-	"code.google.com/p/rsc/qr"
-	"code.google.com/p/rsc/qr/coding"
-	"code.google.com/p/rsc/qr/web/resize"
+	"github.com/odeke-em/rsc/appfs/fs"
+	"github.com/odeke-em/rsc/gf256"
+	"github.com/odeke-em/rsc/qr"
+	"github.com/odeke-em/rsc/qr/coding"
+	"github.com/odeke-em/rsc/qr/web/resize"
 )
 
 func runTemplate(c *fs.Context, w http.ResponseWriter, name string, data interface{}) {
